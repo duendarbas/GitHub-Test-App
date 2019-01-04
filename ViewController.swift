a72405f1b8c49adc9b22ea904a76d2669c4efbc8
duendarbas <46337822+duendarbas@users.noreply.github.com>
2019-01-04 07:50:39 +0100

    Main files

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Test App
+//
+//  Created by Dündar Bas on 04.01.19.
+//  Copyright © 2019 Duendar Bas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
